4a97815de1aaace2de89d7261f8f91890021874c
Kenny <kenny_lee_1@hotmail.com>
2021-03-26 02:22:23 +0100

    step 2

---
+++ Inlämningsuppgift1/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift1.Models
+{
+    public class Event
+    {
+        public int Event_id { get; set; }
+        public string Title { get; set; }
+        public Organizer Organizer { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Adress { get; set; }
+        public int Date { get; set; }
+        public string Spots_avaible { get; set; }
+        public List<Attendee> Attendee { get; set; }
+
+    }
+}
